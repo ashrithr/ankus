6522290d4265460a428089f61c0adbad39bc3691
ashrith <ashrith@cloudwick.com>
2013-07-16 23:19:19 -0700

    print patches

--- lib/ankuscli/config_parser.rb
+++ lib/ankuscli/config_parser.rb
@@ -216,12 +216,12 @@ module Ankuscli
           exit 1
         end
         if slave_nodes_storage_capacity.nil?
-          puts '[Debug]' + ' (Warning) '.yellow + 'if slave_nodes_storage_capacity is not specified no volumes will be created and attached' if @debug
+          puts '[Debug]:' + ' (Warning) '.yellow + 'if slave_nodes_storage_capacity is not specified no volumes will be created and attached' if @debug
         elsif ! slave_nodes_storage_capacity.is_a?(Numeric)
           puts '[Error]: '.red + 'expecting numeric value for slave_nodes_storage_capacity'
           exit 1
         elsif slave_nodes_storage_capacity == 0
-          puts '[Debug]' + ' (Warning) '.yellow + 'slave_nodes_storage_capacity is zero, no volumes will be created and attached to cloud instances' if @debug
+          puts '[Debug]:' + ' (Warning) '.yellow + 'slave_nodes_storage_capacity is zero, no volumes will be created and attached to cloud instances' if @debug
         end
       end
 
