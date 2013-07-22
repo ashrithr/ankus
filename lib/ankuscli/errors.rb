1cf71da6f2083be14b4ea91e9db2eb6ca2798fc8
ashrithr <ashrith@me.com>
2013-07-21 21:07:53 -0700

    Errors baseline

---
+++ lib/ankuscli/errors.rb
@@ -0,0 +1,16 @@
+module Ankuscli
+  module Errors
+    class Error < StandardError
+      attr_accessor :verbose
+
+      def self.slurp(error, message=nil)
+        new_error = new(message)
+        new_error.set_backtrace(error.backtrace)
+        new_error.verbose = error.message
+        new_error
+      end
+    end
+    class NotImplemented < Ankuscli::Errors::Error; end
+    class ParseError < Ankuscli::Errors::Error; end
+  end
+end
